ed9355355140929dc09900b40959d0b2f091409c
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-11-14 22:17:51 +0100

    Added calculation for points per user. Added user list. Added requirement of being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  rescue_from SecurityError, with: :not_signed_in
+  helper_method :nick
+  helper_method :verify_access
 
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
@@ -9,4 +12,52 @@ class ApplicationController < ActionController::Base
       @provider.present?
     end
   end
+
+  def verify_access
+    if not cookies[:chalmersItAuth].present?
+      raise SecurityError, "Missing cookie"
+    end
+    NickHelper.get_user cookies[:chalmersItAuth]
+  end
+
+  def nick cid
+  	NickHelper.nick cid
+  end
+
+  class NickHelper
+
+		include HTTParty
+		base_uri "https://account.chalmers.it/userInfo.php"
+
+		def self.nick cid
+			Rails.cache.fetch "#{cid}/nick", expires_in: 24.hours do
+				resp = send_request(query: { cid: cid })
+				if resp['nick'].present?
+					resp['nick']
+				else
+					cid
+				end
+			end
+		end
+
+		def self.get_user token
+			send_request(query: {token: token})
+		end
+
+		private
+			def self.send_request(options)
+				resp = get("", options)
+				if resp.success? && resp['cid'].present?
+					resp
+				else
+					raise SecurityError, resp['error']
+				end
+			end
+	end	
+
+	private
+       def not_signed_in
+         redirect_to "https://account.chalmers.it?redirect_to=#{request.original_url}"
+       end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :achievements, only: [:index, :create, :edit, :update, :destroy, :show]
   resources :unlocks, only: [:index, :create, :destroy, :show]
-  resources :users, only: :show
+  resources :users, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
